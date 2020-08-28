9d21e7d001eac768b154514e8dd398e1abfc1426
Vonce <vincerosane@outlook.com>
2020-08-27 17:08:08 -0700

    Now able to handle playlists

--- commands/play.js
+++ commands/play.js
@@ -1,90 +1,112 @@
 const ytdl = require("ytdl-core");
+const ytlist = require("youtube-playlist");
 
 module.exports = {
-  name: "play",
-  description: "Play a song in your channel!",
-  async execute(message) {
-    try {
-      const args = message.content.split(" ");
-      const queue = message.client.queue;
-      console.log('The queue is: ' + queue);
-      const serverQueue = message.client.queue.get(message.guild.id);
-      console.log('The serverQueue is: ' + serverQueue);
-
-      const voiceChannel = message.member.voice.channel;
-      console.log(`voiceChannel: ${voiceChannel}`);
-      if (!voiceChannel)
-        return message.channel.send(
-          "You need to be in a voice channel to play music!"
-        );
-      const permissions = voiceChannel.permissionsFor(message.client.user);
-      if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-          "I need the permissions to join and speak in your voice channel!"
-        );
-      }
-
-
-      const songInfo = await ytdl.getInfo(args[1]);
-      const song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url
-      };
-
-      if (!serverQueue) {
-        const queueContruct = {
-          textChannel: message.channel,
-          voiceChannel: voiceChannel,
-          connection: null,
-          songs: [],
-          volume: 5,
-          playing: true
-        };
-        console.log(`Server queue wasn't found, making a new one: ${serverQueue}\nqueueContruct: ${queueContruct}`);
-
-        queue.set(message.guild.id, queueContruct);
-        queueContruct.songs.push(song);
-
+    name: "play",
+    description: "Play a song in your channel!",
+    async execute(message) {
         try {
-          var connection = await voiceChannel.join();
-          queueContruct.connection = connection;
-          this.play(message, queueContruct.songs[0]);
-        } catch (err) {
-          console.log(err);
-          queue.delete(message.guild.id);
-          return message.channel.send(err);
+            const queue = message.client.queue;
+            const args = message.content.split(" ");
+            if (!args[1]) return message.channel.send('??? There\'s no link brother');
+            //console.log('The queue is: ' + queue);
+            const serverQueue = message.client.queue.get(message.guild.id);
+            //console.log('The serverQueue is: ' + serverQueue);
+
+            const voiceChannel = message.member.voice.channel;
+            console.log(`voiceChannel: ${voiceChannel}`);
+            if (!voiceChannel)
+                return message.channel.send(
+                    "You\'re not even in a channel dude"
+                );
+            const permissions = voiceChannel.permissionsFor(message.client.user);
+            if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+                return message.channel.send(
+                    "I need the permissions to join and speak in your voice channel!"
+                );
+            }
+            // message.channel.send(`serverQueue ${serverQueue}`)
+            if (!serverQueue) {
+                const queueContruct = {
+                    textChannel: message.channel,
+                    voiceChannel: voiceChannel,
+                    connection: null,
+                    songs: [],
+                    volume: 5,
+                    playing: true
+                };
+
+                try {
+                    await ytlist(args[1], ['name', 'url']).then(res => {
+                        return res.data.playlist;
+                    }).then(arr => {
+                        message.channel.send(`Found a playlist with ${arr.length} songs`);
+                        for (let i = 0; i < arr.length; i++) {
+
+                            var playlist = {
+                                title: arr[i].name,
+                                url: arr[i].url,
+                            };
+                            console.log(`${playlist.title} - ${playlist.url}`)
+                            queueContruct.songs.push(playlist);
+                        }
+                        queue.set(message.guild.id, queueContruct);
+                        message.channel.send(`**Added ${arr.length} tracks**`);
+                    })
+                } catch {
+                    let info = await ytdl.getInfo(args[1]);
+                    let song = {
+                        title: info.videoDetails.title,
+                        url: info.videoDetails.video_url,
+                    };
+                    console.log(`Pushing song: ${song.title}\n${song.url}`);
+                    queue.set(message.guild.id, queueContruct);
+                    queueContruct.songs.push(song);
+                }
+
+
+                try {
+                    // message.channel.send(`Awaiting to join`)
+                    var connection = await voiceChannel.join();
+                    queueContruct.connection = connection;
+                    this.play(message, queueContruct.songs[0]);
+                } catch (err) {
+                    console.log(err);
+                    queue.delete(message.guild.id);
+                    return message.channel.send(err);
+                }
+
+            } else {
+                serverQueue.songs.push(song);
+                return message.channel.send(
+                    `${song.title} has been added`
+                );
+            }
+        } catch (error) {
+            console.log(error);
+            message.channel.send(error.message);
         }
-      } else {
-        serverQueue.songs.push(song);
-        return message.channel.send(
-          `${song.title} has been added to the queue!`
-        );
-      }
-    } catch (error) {
-      console.log(error);
-      message.channel.send(error.message);
+    },
+
+    play(message, song) {
+        const queue = message.client.queue;
+        const guild = message.guild;
+        const serverQueue = queue.get(message.guild.id);
+
+        if (!song) {
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+
+        const dispatcher = serverQueue.connection
+            .play(ytdl(song.url, { filter: 'audioonly' }))
+            .on("finish", () => {
+                serverQueue.songs.shift();
+                this.play(message, serverQueue.songs[0]);
+            })
+            .on("error", error => console.error(error));
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+        serverQueue.textChannel.send(`Now Playing: **${song.title}**`);
     }
-  },
-
-  play(message, song) {
-    const queue = message.client.queue;
-    const guild = message.guild;
-    const serverQueue = queue.get(message.guild.id);
-
-    if (!song) {
-      serverQueue.voiceChannel.leave();
-      queue.delete(guild.id);
-      return;
-    }
-
-    const dispatcher = serverQueue.connection
-      .play(ytdl(song.url, {filter: 'audioonly'}))
-      .on("finish", () => {
-        serverQueue.songs.shift();
-        this.play(message, serverQueue.songs[0]);
-      })
-      .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-  }
 };
--- commands/stop.js
+++ commands/stop.js
@@ -1,15 +1,15 @@
 module.exports = {
-	name: 'stop',
+    name: 'stop',
     description: 'Stop all songs in the queue',
-	execute(message) {
+    execute(message) {
         const serverQueue = message.client.queue.get(message.guild.id);
-        if(!message.guild.me.voice.channel) return message.channel.send('Yo I\'m not even there');
-        if(serverQueue){
+        if (!message.guild.me.voice.channel) return message.channel.send('Yo I\'m not even there');
+        if (serverQueue) {
             if (!message.member.voice.channel) return message.channel.send('Can\'t really stop it if you\'re not here :/');
             serverQueue.songs = [];
             serverQueue.connection.dispatcher.end();
         } else {
             return message.channel.send('Nothing is playing right now');
-        } 
-	}
+        }
+    }
 };
