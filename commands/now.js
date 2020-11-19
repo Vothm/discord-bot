b39f1506ea1801e0c0920528f22a60cd92b29686
Vonce <vincerosane@outlook.com>
2020-11-19 03:22:27 -0800

    Update

--- commands/now.js
+++ commands/now.js
@@ -1,60 +1,5 @@
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
-module.exports = {
-	name: 'now',
-	description: 'Check playing of audio',
-	async execute(message) {
-		let serverQueue = message.client.queue.get(message.guild.id);
-		const recievedEmbed = message.embeds[0];
-
-		if (serverQueue) {
-			try {
-				return new Promise(async (resolve, reject) => {
-					await ytdl.getInfo(serverQueue.songs[0].url).then((res) => {
-						const songInfo = {
-							title: res.player_response.videoDetails.title,
-							url: res.video_url,
-							shortDescription: truncateString(res.description, 200),
-							id: res.video_id,
-							author: res.author.name
-						};
-
-						const currentMusic = new Discord.MessageEmbed(recievedEmbed)
-							.setColor('#FF0000')
-							.setTitle(`Now playing: ${songInfo.title}`)
-							.setURL(songInfo.url)
-							.setDescription(songInfo.shortDescription)
-							.setThumbnail(`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`)
-							.setFooter(
-								`${songInfo.title}\n${serverQueue.songs.length - 1} songs left`,
-								`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`
-							)
-							.setImage(`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`)
-							.setAuthor(songInfo.author);
-
-						// message.channel.send({ embed: currentMusic }).then(react => {
-						//     Promise.all([
-						//         react.react('⏭️'),
-						//         react.react('⏯️'),
-						//     ])
-						// });
-						//serverQueue.on('')
-						resolve(currentMusic);
-					});
-				});
-			} catch (error) {
-				console.log('Could not get song info ' + error);
-			}
-		} else {
-			message.channel.send('Nothing is in the queue right now');
-		}
-
-		function truncateString(str, num) {
-			if (str.length <= num) return str;
-			else return str.slice(0, num) + '...';
-		}
-	}
-};
 
 module.exports = {
 	name: 'now',
@@ -66,37 +11,36 @@ module.exports = {
 		if (serverQueue) {
 			try {
 				return new Promise(async (resolve, reject) => {
-					await ytdl.getInfo(serverQueue.songs[0].url).then((res) => {
-						const songInfo = {
-							title: res.player_response.videoDetails.title,
-							url: res.video_url,
-							shortDescription: truncateString(res.description, 200),
-							id: res.video_id,
-							author: res.author.name
-						};
+					let info = await ytdl.getInfo(serverQueue.songs[0].url);
+					const songInfo = {
+						title: info.player_response.videoDetails.title,
+						url: info.video_url,
+						shortDescription: truncateString(info.description, 200),
+						id: info.video_id,
+						author: info.author.name
+					};
 
-						const currentMusic = new Discord.MessageEmbed(recievedEmbed)
-							.setColor('#FF0000')
-							.setTitle(`Now playing: ${songInfo.title}`)
-							.setURL(songInfo.url)
-							.setDescription(songInfo.shortDescription)
-							.setThumbnail(`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`)
-							.setFooter(
-								`${songInfo.title}\n${serverQueue.songs.length - 1} songs left`,
-								`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`
-							)
-							.setImage(`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`)
-							.setAuthor(songInfo.author);
+					const currentMusic = new Discord.MessageEmbed(recievedEmbed)
+						.setColor('#FF0000')
+						.setTitle(`Now playing: ${songInfo.title}`)
+						.setURL(songInfo.url)
+						.setDescription(songInfo.shortDescription)
+						.setThumbnail(`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`)
+						.setFooter(
+							`${songInfo.title}\n${serverQueue.songs.length - 1} songs left`,
+							`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`
+						)
+						.setImage(`https://img.youtube.com/vi/${songInfo.id}/maxresdefault.jpg`)
+						.setAuthor(songInfo.author);
 
-						// message.channel.send({ embed: currentMusic }).then(react => {
-						//     Promise.all([
-						//         react.react('⏭️'),
-						//         react.react('⏯️'),
-						//     ])
-						// });
-						//serverQueue.on('')
-						resolve(currentMusic);
-					});
+					// message.channel.send({ embed: currentMusic }).then(react => {
+					//     Promise.all([
+					//         react.react('⏭️'),
+					//         react.react('⏯️'),
+					//     ])
+					// });
+					//serverQueue.on('')
+					resolve(currentMusic);
 				});
 			} catch (error) {
 				console.log('Could not get song info ' + error);
--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,7 @@
 const ytdl = require('ytdl-core');
 const ytlist = require('youtube-playlist');
 const Discord = require('discord.js');
+const now = require('./now');
 
 module.exports = {
 	name: 'play',
@@ -9,7 +10,7 @@ module.exports = {
 		try {
 			const queue = message.client.queue;
 			const args = message.content.split(' ');
-			let validate = ytdl.validateURL(args[1]);
+			let validate = await ytdl.validateURL(args[1]);
 			if (!args[1]) return message.channel.send("??? There's no link brother");
 			if (!validate) return message.channel.send("That's not even a proper link bro");
 			const serverQueue = message.client.queue.get(message.guild.id);
@@ -22,15 +23,9 @@ module.exports = {
 			if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
 				return message.channel.send('I need the permissions to join and speak in your voice channel!');
 			}
-
-			const songInfo = await ytdl.getInfo(args[1]);
-			const song = {
-				title: songInfo.videoDetails.title,
-				url: songInfo.videoDetails.video_url
-			};
-
+			// If there is no music in the queue, make a contract that containts properties for the connection
 			if (!serverQueue) {
-				const queueContruct = {
+				const queueContract = {
 					textChannel: message.channel,
 					voiceChannel: voiceChannel,
 					connection: null,
@@ -38,141 +33,188 @@ module.exports = {
 					volume: 5,
 					playing: true
 				};
+
 				try {
-					await ytlist(args[1], [ 'name', 'url' ])
-						.then((res) => {
-							return res.data.playlist;
-						})
-						.then((arr) => {
-							//message.channel.send(`Found a playlist with ${arr.length} songs`);
-							for (let i = 0; i < arr.length; i++) {
-								var playlist = {
-									title: arr[i].name,
-									url: arr[i].url
-								};
-								console.log(`${playlist.title} - ${playlist.url}`);
-								queueContruct.songs.push(playlist);
-							}
-							queue.set(message.guild.id, queueContruct);
-							message.channel.send(`**Added ${arr.length} tracks**`);
-						});
+					await addSongs(args[1], queueContract);
 				} catch (error) {
-					console.log('Unable to add the playlist');
-					let info = await ytdl.getInfo(args[1]);
-					let song = {
-						title: info.videoDetails.title,
-						url: info.videoDetails.video_url
-					};
-					console.log(`Pushing song: ${song.title}\n${song.url}`);
-					queue.set(message.guild.id, queueContruct);
-					queueContruct.songs.push(song);
+					console.log('Failed to add songs to the queueContract');
 				}
+
+				// Add songs: automatically checks if it's a single or playlist link
+				queue.set(message.guild.id, queueContract);
+				// Connect the bot to voice channel and play music
 				try {
-					// message.channel.send(`Awaiting to join`)
-					var connection = await voiceChannel.join();
-					queueContruct.connection = connection;
-					this.play(message, queueContruct.songs[0]);
-					// message.channel.send(`queueContruct songs ${queueContruct.songs[0].title}\n${queueContruct.songs[0].url}`)
+					const connection = await message.member.voice.channel.join();
+					queueContract.connection = connection;
+					play(message, queueContract.songs[0]);
 				} catch (err) {
 					console.log(err);
 					queue.delete(message.guild.id);
 					return message.channel.send(err);
 				}
 			} else {
-				try {
-					await ytlist(args[1], [ 'name', 'url' ])
-						.then((res) => {
-							return res.data.playlist;
-						})
-						.then((arr) => {
-							for (let i = 0; i < arr.length; i++) {
-								let songFromPlaylist = {
-									title: arr[i].name,
-									url: arr[i].url
-								};
-								serverQueue.songs.push(songFromPlaylist);
-							}
-							message.channel.send(`**Added ${arr.length} items to the queue**`);
-						});
-				} catch (error) {
-					serverQueue.songs.push(song);
-					message.channel.send(`**Added ${song.title} to the queue**`);
-				}
+				// Assume that server contract is already made, therefore just add song/playlist to the queue
+				await addSongs(args[1], serverQueue);
 			}
 		} catch (error) {
-			console.log(error);
-			message.channel.send(error.message);
-		}
-	},
-
-	async play(message, song) {
-		const queue = message.client.queue;
-		const guild = message.guild;
-		const serverQueue = queue.get(message.guild.id);
-
-		if (!song) {
-			message.channel.send('There are no more songs in the queue');
-			serverQueue.voiceChannel.leave();
-			queue.delete(guild.id);
-			return;
+			console.error(error);
 		}
 
-		try {
-			if (serverQueue) {
-				let info = await ytdl.getInfo(serverQueue.songs[0].url);
-				let recievedEmbed = message.embeds[0];
-				let currentMusic = new Discord.MessageEmbed(recievedEmbed)
-					.setColor('#FF0000')
-					.setTitle(`Now playing: ${info.videoDetails.title}`)
-					.setURL(info.videoDetails.video_url)
-					//.setDescription(info.videoDetails.shortDescription)
-					.setThumbnail(`https://img.youtube.com/vi/${info.videoDetails.videoId}/maxresdefault.jpg`)
-					.setFooter(
-						`${info.videoDetails.title}\n${serverQueue.songs.length - 1} songs left`,
-						`https://img.youtube.com/vi/${info.videoDetails.videoId}/maxresdefault.jpg`
-					)
-					.setImage(`https://img.youtube.com/vi/${info.videoDetails.videoId}/maxresdefault.jpg`);
-
-				const filter = (reaction, user) => {
-					return [ '⏭️', '▶️', '⏸️' ].includes(reaction.emoji.name) && user.id === message.author.id;
-				};
-				message.channel.send({ embed: currentMusic }).then(async (react) => {
-					react.react('⏭️');
-					react.react('⏸️');
-					react.react('▶️');
-					const dispatcher = serverQueue.connection
-						.play(ytdl(song.url, { quality: 'highestaudio', filter: 'audioonly', highWaterMark: 1 << 25 }))
-						.on('finish', () => {
-							serverQueue.songs.shift();
-							//message.channel.delete({ embed: currentMusic });
-							this.play(message, serverQueue.songs[0]);
-							react.delete({ timeout: 500 });
-							dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-						})
-						.on('error', (error) => console.error(error));
-
-					react
-						.awaitReactions(filter, { max: 1 })
-						.then(async (collected) => {
-							const reaction = collected.first();
-							if (reaction.emoji.name === '⏭️') {
-								serverQueue.songs.shift();
-								react.reactions
-									.removeAll()
-									.catch((error) => console.error('Failed to clear reactions: ', error));
-								react.delete({ timeout: 500 });
-								this.play(message, serverQueue.songs[0]);
-							}
-						})
-						.catch((collected) => {
-							console.log('Idk what happened ' + collected);
-						});
+		// function to add songs to a specific queue. The playlist can only return 100 songs but I don't think anyone is gonna queue up > 100 anyway.
+		async function addSongs(url, queue) {
+			try {
+				await ytlist(url, [ 'url', 'name' ]).then((res) => {
+					for (let i = 0; i < res.data.playlist.length; i++) {
+						const song = {
+							title: res.data.playlist[i].name,
+							url: res.data.playlist[i].url
+						};
+						queue.songs.push(song);
+					}
+					message.channel.send(`**Added ${res.data.playlist.length} tracks**`);
 				});
-			} else {
-				message.channel.send('Nothing else in the queue');
+			} catch (error) {
+				await ytdl.getInfo(url).then((res) => {
+					const song = {
+						title: res.player_response.videoDetails.title,
+						url: res.video_url
+					};
+					message.channel.send(`**Added ${song.title} to the queue**`);
+					queue.songs.push(song);
+				});
 			}
-		} catch (err) {
-			message.channel.send('You fucked up');
+		}
+
+		async function play(message, song) {
+			const queue = message.client.queue;
+			const guild = message.guild;
+			const serverQueue = queue.get(message.guild.id);
+			//message.channel.send(`message.guild.me ${message.guild.me.id}`)
+
+			if (!song) {
+				message.channel.send('There are no more songs in the queue');
+				message.guild.me.voice.channel.leave();
+				queue.delete(guild.id);
+				return;
+			}
+
+			try {
+				if (serverQueue) {
+					//let info = await ytdl.getInfo(serverQueue.songs[0].url);
+					let card = await now.execute(message);
+
+					const filter = (reaction, user) => {
+						return [ '⏭️', '⏯️' ].includes(reaction.emoji.name) && user.id !== message.guild.me.id;
+					};
+					message.channel.send({ embed: card }).then(async (react) => {
+						Promise.all([ react.react('⏭️'), react.react('⏯️') ])
+							.catch(() => console.error('One of the emojis failed to react.'))
+							.then(async () => {
+								const dispatcher = serverQueue.connection
+									.play(await ytdl(song.url), {
+										type: 'opus',
+										filter: 'audioonly',
+										//highWaterMark: 1 << 10,
+										quality: 'highestaudio'
+									})
+									.on('finish', () => {
+										serverQueue.songs.shift();
+										play(message, serverQueue.songs[0]);
+										react.delete({ timeout: 500 });
+										dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+									})
+									.on('error', (error) => {
+										react.delete({ timeout: 500 });
+										serverQueue.songs.shift();
+										play(message, serverQueue.songs[0]);
+										console.log(`Dispatcher error` + error);
+									});
+
+								// Useful only if you know how many reactions you want
+								// react.awaitReactions(filter, { max: 1 }).then(async collected => {
+								//     const reaction = collected.first();
+								//     if (reaction.emoji.name === '⏭️') {
+								//         serverQueue.songs.shift();
+								//         react.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+								//         react.delete({ timeout: 500 });
+								//         play(message, serverQueue.songs[0]);
+								//     }
+								// }).catch(collected => {
+								//     console.log('Idk what happened ' + collected);
+								// });
+
+								const collector = react.createReactionCollector(filter);
+								collector.on('collect', async (reaction, user) => {
+									if (reaction.emoji.name === '⏭️') {
+										let userId = user.id;
+										if (!message.guild.member(userId).voice.channel) {
+											const userReactions = react.reactions.cache.filter((rec) =>
+												rec.users.cache.has(userId)
+											);
+											try {
+												for (const rec of userReactions.values()) {
+													await rec.users.remove(userId);
+												}
+											} catch (error) {
+												console.error('Failed to remove');
+											}
+											return message.channel.send(`${user} Bitch you tried`);
+										}
+										serverQueue.songs.shift();
+										react.delete({ timeout: 500 });
+										play(message, serverQueue.songs[0]);
+									}
+
+									if (reaction.emoji.name === '⏯️') {
+										let userId = user.id;
+										const userReactions = react.reactions.cache.filter((rec) =>
+											rec.users.cache.has(userId)
+										);
+										//message.channel.send(`userID ${userId}\nuser ${user}\nMessageMember${message.member}\nid ${message.guild.member(user.id).voice.channel.id}`)
+										if (!message.guild.member(userId).voice.channel) {
+											try {
+												for (const rec of userReactions.values()) {
+													await rec.users.remove(userId);
+												}
+											} catch (error) {
+												console.error('Failed to remove');
+											}
+											return message.channel.send(`${user} Bitch you tried`);
+										}
+
+										try {
+											for (const rec of userReactions.values()) {
+												await rec.users.remove(userId);
+											}
+										} catch (error) {
+											console.error('Failed to remove');
+										}
+
+										if (dispatcher.paused) {
+											//message.channel.send('Resuming');
+											dispatcher.resume();
+										} else {
+											dispatcher.pause();
+										}
+									}
+								});
+							});
+					});
+				} else {
+					message.channel.send('Nothing else in the queue');
+				}
+			} catch (err) {
+				serverQueue.songs.shift();
+				console.log('You fucked up ' + err + '\nPlaying next song...');
+				play(message, serverQueue.songs[0]);
+			}
+
+			function truncateString(str, num) {
+				if (str.length <= num) {
+					return str;
+				}
+				return str.slice(0, num) + '...';
+			}
 		}
 	}
 };
